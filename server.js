9d0d6f942451511355d2c21af64f28bd216dfb92
Mary Scale <scalemary@gmail.com>
2016-10-24 19:05:40 +0000

    created /hello path for the server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/hello', function(require, response){
+   response.send('<h1>Hello World!</h1>'); 
+});
+
+
+/* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
+
+// Boilerplate code to start up the web server
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
